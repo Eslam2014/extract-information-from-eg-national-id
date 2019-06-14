e49f0f5d84dfcd0beab932cc1c68e01a26449cfc
eslam2014 <abkarino.em2012@gamil.com>
2019-06-14 22:58:30 +0200

    Refactor the code

--- extractor.py
+++ extractor.py
@@ -28,135 +28,149 @@ script usage usage: extractor.py [-h] national_id
 import argparse
 from datetime import datetime
 import textwrap
+from collections import namedtuple
+import sys
 
-governorates = {'01': 'Cairo',
-                '02': 'Alexandria',
-                '03': 'Port Said',
-                '04': 'Suez',
-                '11': 'Damietta',
-                '12': 'Dakahlia',
-                '13': 'Ash Sharqia',
-                '14': 'Kaliobeya',
-                '15': 'Kafr El - Sheikh',
-                '16': 'Gharbia',
-                '17': 'Monoufia',
-                '18': 'El Beheira',
-                '19': 'Ismailia',
-                '21': 'Giza',
-                '22': 'Beni Suef',
-                '23': 'Fayoum',
-                '24': 'El Menia',
-                '25': 'Assiut',
-                '26': 'Sohag',
-                '27': 'Qena',
-                '28': 'Aswan',
-                '29': 'Luxor',
-                '31': 'Red Sea',
-                '32': 'New Valley',
-                '33': 'Matrouh',
-                '34': 'North Sinai',
-                '35': 'South Sinai',
-                '88': 'Foreign'}
+class ValidationError(Exception):
+    def __init__(self, message, errors):
+        super(ValidationError, self).__init__(message)
+        self.errors = errors
 
-fake_national_id_message = 'This ID Not Valid'
+_ENID = namedtuple('_ENID', ['birth_century', 'date_of_birth', 'birth_governorate', 'sequence_in_computer', 'gender'])
 
-def extract_birth_century(birth_century_code: int) -> int:
+
+class EgyptionNationalId(_ENID):
+    """"
+    Model an Egyptian National ID string.
+
+    >> id = EgyptianNationalId('29501023201952')
+    >> 1999 in id.century
+    True
+    >> id.birth_date.year == 1995
+    True
+    >> id.birth_date.month
+    1
+    >> id.birth_date.day
+    2
+    >> id.birth_date.governorate
+    'New Valley'
     """
-     extract birth century from national id it's in index 0
-    :param birth_century_code: one digit
-    :return: birth century
-    """
-    assert type(birth_century_code) == int, 'birth century code must be int value'
-    current_century = get_century_from_year(int(datetime.now().year))
-    birth_century = birth_century_code + 18
-    assert (birth_century >= 19) and (birth_century <= current_century), fake_national_id_message
-    return birth_century
+    fake_national_id_message = 'This National ID Not Valid'
+    governorates = {'01': 'Cairo',
+                    '02': 'Alexandria',
+                    '03': 'Port Said',
+                    '04': 'Suez',
+                    '11': 'Damietta',
+                    '12': 'Dakahlia',
+                    '13': 'Ash Sharqia',
+                    '14': 'Kaliobeya',
+                    '15': 'Kafr El - Sheikh',
+                    '16': 'Gharbia',
+                    '17': 'Monoufia',
+                    '18': 'El Beheira',
+                    '19': 'Ismailia',
+                    '21': 'Giza',
+                    '22': 'Beni Suef',
+                    '23': 'Fayoum',
+                    '24': 'El Menia',
+                    '25': 'Assiut',
+                    '26': 'Sohag',
+                    '27': 'Qena',
+                    '28': 'Aswan',
+                    '29': 'Luxor',
+                    '31': 'Red Sea',
+                    '32': 'New Valley',
+                    '33': 'Matrouh',
+                    '34': 'North Sinai',
+                    '35': 'South Sinai',
+                    '88': 'Foreign'}
 
+    @classmethod
+    def from_str(cls, national_id):
+        fields = cls.parse_str(national_id)
+        return cls(*fields)
 
-def get_century_from_year(year):
-    return year // 100 + 1
+    @classmethod
+    def parse_str(cls, national_id):
+        if len(national_id) != 14 or national_id.isdigit() is False:
+            print("Hi there")
+            raise ValidationError('National id format not valid', cls.fake_national_id_message)
+        birth_century = cls.__get_birth_century(cls,int(national_id[0]))
+        date_of_birth = cls.__convert_birthdate(cls ,national_id[0:7])
+        birth_governorate = cls.__get_birth_governorate(cls, national_id[7:9])
+        sequence_in_computer = national_id[9:13]
+        gender = cls.__get_gender(cls, int(national_id[12]))
 
+        fields = (birth_century,
+                  date_of_birth,
+                  birth_governorate,
+                  sequence_in_computer,
+                  gender)
+        return fields
 
-def convert_birthdate(birthdate: str) -> str:
-    """
-    Convert birthday in national id from  fromat yymmdd to yyyy - mm - dd format
-    it's from index 0 to 6 in EG national id
-    :param birthdate: str
-            format cyymmdd, c represent birth century code
-    :return: str
-            yyyy-mm-dd
-    """
-    assert len(str(birthdate)) == 7, "birthdate must be 7 digit"
-    birth_century = extract_birth_century(int(birthdate[0]))
-    birth_year = birthdate[1:3]
-    birth_month = birthdate[3:5]
-    birth_day = birthdate[5:]
-    birth_full_year = (birth_century * 100) - 100 + int(birth_year)
-    birthdate_str = '{0}-{1}-{2}'.format(birth_full_year, birth_month, birth_day)
-    birthdate_date = datetime.strptime(birthdate_str, '%Y-%m-%d')
-    assert birthdate_date <= datetime.now() and birthdate_date >= datetime.strptime('1900-01-01','%Y-%m-%d'), fake_national_id_message
-    return birthdate_str
+    def __get_century_from_year(self, year):
+        return year // 100 + 1
 
+    def __get_birth_century(self, birth_century_code):
+        """
+         get birth century from national id it's in index 0
+        :param birth_century_code: one digit
+        :return: birth century
+        """
 
-def get_birth_governorate(birth_governorate_coda: str) -> str:
-    """
-    :param birth_governorate_coda:
-            Index 7 and 8 in EG national id
-    :return: str
-             Birth governorate
+        current_century = self.__get_century_from_year(self, int(datetime.now().year))
+        birth_century = birth_century_code + 18
 
-    """
-    assert type(birth_governorate_coda) == str, 'Birth governorate coda must be string not integer'
-    assert len(birth_governorate_coda) == 2, 'Birth governorate coda must be 2 digit'
-    assert birth_governorate_coda in governorates, 'Birth governorate coda not valid'
-    return governorates[birth_governorate_coda]
+        if (birth_century < 19) and (birth_century > current_century):
+            raise ValidationError('birth century not valid', self.fake_national_id_message)
+        return birth_century
 
+    def __get_birth_governorate(self, birth_governorate_coda):
+        """
+        :param birth_governorate_coda:
+                Index 7 and 8 in EG national id
+        :return: str
+                 Birth governorate
+        """
+        try:
+            return self.governorates[birth_governorate_coda]
+        except:
+            raise ValidationError('birth governorate code not valid', sys.exc_info()[0])
 
-def get_gender(gender_code: int) -> str:
-    """
-    :param gender_code:
-            Index 12 in EG National ID
+    def __get_gender(self, gender_code):
+        """
+        :param gender_code:
+                Index 12 in EG National ID
 
-    :return: str
-            Gender
-    """
-    assert type(gender_code) == int and gender_code > 0 and gender_code <= 9, 'gender code not valid'
-    if gender_code % 2 == 0:
-        return 'Female'
-    else:
-        return 'Male'
+        :return: str
+                Gender
+        """
+        if gender_code < 0 and gender_code > 9:
+            raise ValidationError('gender code not valid', self.fake_national_id_message)
+        if gender_code % 2 == 0:
+            return 'Female'
+        else:
+            return 'Male'
 
-
-def extract_info_from_national_id(national_id: int):
-    """
-    Extract information from national EG national ID
-    :param national_id: int
-            EG national id must be number of 14 digit
-    :return: dict
-        birth_century
-        date_of_birth
-        birth_governorate
-        sequence_in_computer
-        gender
-    """
-    assert type(national_id) == int, "National ID must be Numbers not string"
-    assert len(str(national_id)) == 14, "National ID must be 14 Number "
-    national_id_str = str(national_id)
-    info ={}
-    info['birth_century'] = extract_birth_century(int(national_id_str[0]))
-    info['date_of_birth'] = convert_birthdate(national_id_str[0:7])
-    info['birth_governorate'] = get_birth_governorate(national_id_str[7:9])
-    info['sequence_in_computer'] = national_id_str[9:13]
-    info['gender'] = get_gender(int(national_id_str[12]))
-    # last_number = national_id_str[13]
-    return info
-
-
-def print_info(information):
-    print('Birth Century :', information['birth_century'])
-    print('Date Of Birth :', information['date_of_birth'])
-    print('Birth Governorate :', information['birth_governorate'])
-    print('Gender :', information['gender'])
+    def __convert_birthdate(self, birthdate):
+        """
+        Convert birthday in national id from  fromat yymmdd to yyyy - mm - dd format
+        it's from index 0 to 6 in EG national id
+        :param birthdate: str
+                format cyymmdd, c represent birth century code
+        :return: str
+                yyyy-mm-dd
+        """
+        birth_century = self.__get_birth_century(self, int(birthdate[0]))
+        birth_year = birthdate[1:3]
+        birth_month = birthdate[3:5]
+        birth_day = birthdate[5:]
+        birth_full_year = (birth_century * 100) - 100 + int(birth_year)
+        birthdate_str = '{0}-{1}-{2}'.format(birth_full_year, birth_month, birth_day)
+        birthdate_date = datetime.strptime(birthdate_str, '%Y-%m-%d')
+        if birthdate_date > datetime.now() and birthdate_date < datetime.strptime('1900-01-01','%Y-%m-%d'):
+            raise ValidationError('birthdate not valid', sys.exc_info()[0])
+        return birthdate_str
 
 
 if __name__ == '__main__':
@@ -170,8 +184,8 @@ if __name__ == '__main__':
                                                 Gender
                                       
                                      '''))
-    parser.add_argument('national_id', type=int,
+    parser.add_argument('national_id', type=str,
                         help="Add The Egyption National ID 14 Digit")
     args = parser.parse_args()
-    info = extract_info_from_national_id(args.national_id)
-    print_info(info)
+    info = EgyptionNationalId.from_str(args.national_id)
+    print(info)
